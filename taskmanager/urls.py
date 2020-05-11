7128e23d0e6b62a2af14372679015f91cc309998
Alexander Bukharin <alexawm182@gmail.com>
2020-05-11 21:21:54 +0300

    fix django auth login and logout only

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,9 +1,12 @@
 """Taskmanager URL Configuration."""
+
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('taskmanager.tasks.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/login', views.LoginView.as_view(), name='login'),
+    path('auth/logout', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
