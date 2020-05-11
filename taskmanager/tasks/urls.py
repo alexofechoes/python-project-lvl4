91206cef21f9aa58cc043e5973e4b161d3ec0d5a
Alexander Bukharin <alexawm182@gmail.com>
2020-05-11 21:06:47 +0300

    add task prefix for tasks url

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -7,12 +7,12 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='index'),
-    path('new', views.TaskCreateView.as_view(), name='create'),
-    path('<int:pk>', views.TaskDetailView.as_view(), name='detail'),
-    path('<int:pk>/edit', views.TaskUpdateView.as_view(), name='edit'),
-    path('<int:pk>/delete', views.TaskDeleteView.as_view(), name='delete'),
-    path('<int:pk>/progress', views.progress, name='progress'),
-    path('<int:pk>/fulfilled', views.fulfilled, name='fulfilled'),
-    path('<int:pk>/returned', views.returned, name='returned'),
-    path('<int:pk>/canceled', views.canceled, name='canceled'),
+    path('task/create', views.TaskCreateView.as_view(), name='create'),
+    path('task/<int:pk>', views.TaskDetailView.as_view(), name='detail'),
+    path('task/<int:pk>/edit', views.TaskUpdateView.as_view(), name='edit'),
+    path('task/<int:pk>/delete', views.TaskDeleteView.as_view(), name='delete'),
+    path('task/<int:pk>/progress', views.progress, name='progress'),
+    path('task/<int:pk>/fulfilled', views.fulfilled, name='fulfilled'),
+    path('task/<int:pk>/returned', views.returned, name='returned'),
+    path('task/<int:pk>/canceled', views.canceled, name='canceled'),
 ]
